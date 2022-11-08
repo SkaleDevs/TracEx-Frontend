d5e63018ad82ee53e15d533b68858b46403287bf
FuryACE <sudhanshuvshekhar@gmail.com>
2022-11-09 01:52:40 +0530

    init condition

---
+++ src/components/LogoSign/index.tsx
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Tooltip,
+  Badge,
+  TooltipProps,
+  tooltipClasses,
+  styled,
+  useTheme
+} from '@mui/material';
+import Link from 'src/components/Link';
+
+import urjaLogo from 'logo.png';
+import Image from 'next/image';
+
+const LogoWrapper = styled(Link)(
+  ({ theme }) => `
+        color: ${theme.palette.text.primary};
+        display: flex;
+        text-decoration: none;
+        width: 53px;
+        margin: 0 auto;
+        font-weight: ${theme.typography.fontWeightBold};
+`
+);
+
+const LogoSignWrapper = styled(Box)(
+  () => `
+        width: 52px;
+        height: 38px;
+`
+);
+
+const TooltipWrapper = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.colors.alpha.trueWhite[100],
+    color: theme.palette.getContrastText(theme.colors.alpha.trueWhite[100]),
+    fontSize: theme.typography.pxToRem(12),
+    fontWeight: 'bold',
+    borderRadius: theme.general.borderRadiusSm,
+    boxShadow:
+      '0 .2rem .8rem rgba(7,9,25,.18), 0 .08rem .15rem rgba(7,9,25,.15)'
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: theme.colors.alpha.trueWhite[100]
+  }
+}));
+
+function Logo() {
+  const theme = useTheme();
+
+  return (
+    <TooltipWrapper title="Urja Dashboard" arrow>
+      <LogoWrapper href="/">
+        <LogoSignWrapper>
+          <Image src= "/logo.png" width= {150} height= {158} />
+        </LogoSignWrapper>
+      </LogoWrapper>
+    </TooltipWrapper>
+  );
+}
+
+export default Logo;
