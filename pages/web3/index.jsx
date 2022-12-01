9d2190e700910b286fab32f9bc729f1288ec3f9b
Shreya Narayan <shreyanushka02@gmail.com>
2022-12-02 01:37:13 +0530

    finally done

--- pages/web3/index.jsx
+++ pages/web3/index.jsx
@@ -1,34 +1,37 @@
 import React, { useEffect } from 'react';
 import { ethers } from 'ethers';
-import SupplyChainAbi from '../../lib/SupplyChainAbi';
+import SupplyChain from '../../lib/SupplyChainAbi.json';
 
 const TestWeb3 = () => {
   const addProduct = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const SupplyChainContract = new ethers.Contract(
-      '0x0978C8E89849a673BdE627A031574c048a4ac293',
-      SupplyChainAbi,
+      '0xc84C0272Ddced2502Cb257244Bc4EebE851bBcE1',
+      SupplyChain.abi,
       signer
     );
 
-    let products = await SupplyChainContract.addProduct(
-      'Vaccine 1',
-      'shreya',
-      '0x474d4B268F28584179d12B7CCB7A8A886D5A0fd7',
-      12345,
-      12346,
-      false,
-      15000,
-      'SGXxdegshrgfsr',
-      'DHfgft',
-      0,
-      'fhcgfchd',
-      'dghxfdxc',
-      ['gdx', 'FHC'],
-      ['GFc', 'hfdxc']
-    );
-    console.log("Hoo");
+  //   let products = await SupplyChainContract.addProduct([
+  //     'Vaccine 1',
+  //     'shreya',
+  //     '0x474d4B268F28584179d12B7CCB7A8A886D5A0fd7',
+  //     12345,
+  //     12346,
+  //     false,
+  //     15000,
+  //     'SGXxdegshrgfsr',
+  //     'DHfgft',
+  //     0,
+  //     'fhcgfchd',
+  //     'dghxfdxc',
+  //     ['gdx', 'FHC'],
+  //     ['GFc', 'hfdxc']
+  //   ],123456,{gasLimit: 5000000});
+  //   console.log(products);
+  // };
+    let view = await SupplyChainContract.getAllProducts();
+    console.log(view);
   };
   // useEffect(() => {
   //   addProduct();
