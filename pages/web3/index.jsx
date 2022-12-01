4cd89004edb9d26eecb40961f14c4c9d724ed4d2
FuryACE <sudhanshuvshekhar@gmail.com>
2022-12-02 00:13:55 +0530

    kuchh v

--- pages/web3/index.jsx
+++ pages/web3/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import Web3 from 'web3';
-import { SupplyChainAbi } from '../../lib/SupplyChainAbi';
+import { ethers } from 'ethers';
+import SupplyChainAbi from '../../lib/SupplyChainAbi';
 
 const web3 = new Web3(Web3.givenProvider);
 
 const TestWeb3 = () => {
-  return <div>Insert Smart Contract Interaction Object here</div>;
+  const addProduct = async () => {
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const SupplyChainContract = new ethers.Contract(
+      '0x0978C8E89849a673BdE627A031574c048a4ac293',
+      SupplyChainAbi,
+      signer
+    );
+
+    // let products = await
+  };
+
+  return <></>;
 };
 
 export default TestWeb3;
